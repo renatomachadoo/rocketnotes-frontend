7eb461cd66b4c8cee48c2e0d376d3a1722e9cb2b
Renato Machado <81891866+Machadoo7@users.noreply.github.com>
2024-09-24 15:08:39 +0100

    ao fazer signOut dar ser redirecionado para o ecra inicial

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { RiShutDownLine } from "react-icons/ri"
 import { Container, Profile, Logout } from "./styles";
 import { useAuth } from "../../hooks/auth";
@@ -7,9 +8,15 @@ import { api } from "../../services/api";
 import avatarPlaceholder from "../../assets/avatar_placeholder.svg"
 
 export function Header(){
+  const navigate = useNavigate("/")
   const { signOut, user } = useAuth()
 
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+
+  function handleLogout(){
+    navigate("/")
+    signOut()
+  }
 
   return(
     <Container>
@@ -25,7 +32,7 @@ export function Header(){
         </div>
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleLogout}>
         <RiShutDownLine/>
       </Logout>
     </Container>
